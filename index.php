96f23534750c404694824e509d5988cb178bb9de
Tylor Schlosser <tschlosser@mail.greenriver.edu>
2020-04-27 16:42:05 -0400

    Inital commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once('vendor/autoload.php');
+
+//F3 class
+$f3 = Base::instance();
+
+//Route
+$f3->route('GET /', function () {
+    //echo "<h1>Welcome to my food page</h1>";
+    $view = new Template();
+    echo $view->render("views/home.html");
+}
+);
+
+$f3->run();
