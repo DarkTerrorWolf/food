1540520d220e57ad87077b93083c0fa846f038c7
Tylor Schlosser <tschlosser@mail.greenriver.edu>
2020-04-29 17:32:20 -0400

    added summary

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
@@ -36,12 +37,43 @@ $f3->route('GET /lunch',function() {
     echo $view->render("views/lunch.html");
 
 });
-$f3->route('GET /order',function() {
-    //echo "<h1>Welcome to my Breakfast page </h1>";
+//order path
+$f3->route('GET|POST /order',function($f3) {
+
+    //if the form has been submitted
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        var_dump($_POST);
+        //array(2) {["food"]=> "tacos", ["meal"]=> "breakfast" }
+
+        //validate data
+        $meals = array("Breakfast","Lunch","Dinner");
+        if(empty($_POST['food'])){
+            echo "<p>Please enter a food</p>";
+        }
+        if(empty($_POST['meal']|| !in_array($_POST['meal'],$meals))){
+            echo "<p>Please choose a meal time</p>";
+        }
+        //Data is valid
+        else{
+            //Store the data in the session array
+            //start a session
+            session_start();
+            $_SESSION['food'] =$_POST['food'];
+            $_SESSION['meal']=$_POST['meal'];
+            $f3 ->reroute("summary");
+            session_destroy();
+        }
+    }
     $view = new Template();
     echo $view->render("views/orderform.html");
 
 });
+$f3->route('GET /summary',function() {
 
+    $view = new Template();
+    echo $view->render("views/summary.html");
+
+});
 
 $f3->run();
